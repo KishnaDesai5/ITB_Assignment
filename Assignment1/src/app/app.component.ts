f3fcff4864b432644e689da75d28e6e86f642067
KishnaDesai5 <desaikrishna2005@gmail.com>
2025-03-04 09:58:46 +0530

    routing done ro..no.31

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './auth-user/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'login',
+        component : LoginComponent
+    },
+    {
+        path : 'register',
+        component : LoginComponent
+    },
+    {
+        path : 'user',
+        component : LoginComponent
+    }
+];
